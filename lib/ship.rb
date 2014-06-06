5683af1759510ea195f78cfe98f6773443617ce4
juliatan <hello@julia-tan.com>
2014-06-06 21:49:55 +0100

    Added coordinate splitted method

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,10 +1,27 @@
 class Ship
 
-  attr_reader :starting_coord, :length
+  attr_reader :starting_coordinates, :direction, :length
 
   def initialize(starting_coord, direction, length)
-    @starting_coord = starting_coord
+    @starting_coordinates = starting_coord
     @direction = direction
     @length = length
   end
+
+  def coordinates
+    ship_coordinates = []
+    ship_coordinates << starting_coordinates
+  end
+
+  def starting_coordinate_splitter
+    letter = starting_coordinates[0]
+    number = starting_coordinates[1]
+  end
+
+  # def ship_built(direction, length)
+  #   case direction
+  #   when 'north'
+  #     starting_coordinates[0]
+  # end
+
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -9,5 +9,23 @@ describe Ship do
     it 'knows its starting coordinate' do
       expect(ship.starting_coord).to eq 'A1'
     end
+
+    it 'knows its direction' do
+      expect(ship.direction).to eq 'east'
+    end
+
+    it 'knows its length' do
+      expect(ship.length).to eq 2
+    end
+
   end
+
+  context 'under gameplay conditions' do
+
+    it 'should know its coordinates' do
+      expect(ship.coordinates).to eq ['A1', 'A2']
+    end
+  end
+
+
 end
